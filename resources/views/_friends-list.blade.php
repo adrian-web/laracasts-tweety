e4e6caa61ebba27fecb34b19d0937bb3e394cb17
adrian-web <65187349+adrian-web@users.noreply.github.com>
2020-05-25 18:51:53 +0200

    Dynamic Timeline

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -2,12 +2,11 @@
 
 <ul>
     @foreach (range(1,8) as $index)
-        <li class="mb-4">
-            <div class="flex items-center text-sm">
-                <img src= "https://www.w3schools.com/howto/img_avatar.png"
-                class="w-10 rounded-full mr-2" alt="avatar">
-                John Doe
-            </div>
-         </li>
+    <li class="mb-4">
+        <div class="flex items-center text-sm">
+            <img src="https://www.w3schools.com/howto/img_avatar.png" class="w-10 rounded-full mr-2" alt="avatar">
+            Adi
+        </div>
+    </li>
     @endforeach
 </ul>
--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,16 +1,19 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
-    <form action="">
+    <form method="POST" action="/tweets">
+        @csrf
         <textarea name="body" class="w-full" placeholder="What's up doc?"></textarea>
         <hr class="my-4">
 
         <footer class="flex justify-between">
-            <img src= "https://www.w3schools.com/howto/img_avatar.png"
-            class="w-10 rounded-full mr-2" alt="avatar">
+            <img src="https://www.w3schools.com/howto/img_avatar.png" class="w-10 rounded-full mr-2" alt="avatar">
             <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
         </footer>
-        
-    </form>
-</div>
-<div>
 
+    </form>
+
+    @error('body')
+    <p class="text-red-500 text-sm mt-2">
+        {{ $message }}
+    </p>
+    @enderror
 </div>
