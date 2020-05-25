0882e0b14e8a55c632089d63ed5e36a6579a24be
adrian-web <65187349+adrian-web@users.noreply.github.com>
2020-05-25 19:33:34 +0200

    Following functionality

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,11 +1,12 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 
 <ul>
-    @foreach (range(1,8) as $index)
+    @foreach (auth()->user()->follows as $user)
     <li class="mb-4">
         <div class="flex items-center text-sm">
             <img src="https://www.w3schools.com/howto/img_avatar.png" class="w-10 rounded-full mr-2" alt="avatar">
-            Adi
+
+            {{$user->name}}
         </div>
     </li>
     @endforeach
