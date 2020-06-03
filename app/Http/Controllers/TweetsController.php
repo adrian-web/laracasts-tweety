e03db7b7a2475862be36b25fcc78821c4a961c8e
adrian-web <65187349+adrian-web@users.noreply.github.com>
2020-06-03 20:23:26 +0200

    Tweet mentions (notifications) demo

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\UserMentioned;
 use Illuminate\Http\Request;
 use App\Tweet;
 use App\User;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Str;
 
 class TweetsController extends Controller
 {
+    public $matches = [];
+
     public function index()
     {
         return view('tweets.index', [
@@ -21,18 +26,23 @@ class TweetsController extends Controller
             'body'=>['required', 'max:255'],
             'image'=>['file']
         ]);
-        
+
         if (request('image')) {
             $attributes['image'] = request('image')->store('images');
         } else {
             $attributes['image'] = null;
         }
 
-        Tweet::create([
+        $tweet = Tweet::create([
             'user_id' => auth()->id(),
             'body' => $attributes['body'],
             'image' => $attributes['image']
         ]);
+
+        $mentions = $tweet->mentions($tweet->body);
+        Notification::send($mentions, new UserMentioned($tweet));
+        // for single user
+        // request()->user()->notify(new UserMentioned($tweet));
 
         return redirect()->route('home')->with('success', 'Tweet published!');
     }
--- app/Tweet.php
+++ app/Tweet.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Tweet extends Model
 {
@@ -20,5 +21,24 @@ class Tweet extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function mentions($request)
+    {
+        $users = User::all();
+        $mentions = [];
+        foreach ($users as $user) {
+            // ^ means string starts with @
+            if (preg_match("/^@+{$user->username}\b/", $request)) {
+                array_push($mentions, $user);
+            }
+            
+            // \s means string starts with white space
+            if (preg_match("/\s@+{$user->username}\b/", $request)) {
+                array_push($mentions, $user);
+            }
+        }
+        
+        return $mentions;
     }
 }
